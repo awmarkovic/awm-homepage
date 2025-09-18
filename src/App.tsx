5a0216cd866c32e31195bc34846f85af84eaac1e
awmarkovic <amma010@student.kristiania.no>
2025-09-19 01:09:32 +0200

    Add basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./reset.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="page-layout">
+      <header className="header">
+        <img
+          className="avatar"
+          src="public/avatar.jpeg"
+          alt="Picture of Amanda"
+        />
+        <div className="copy-text">
+          <h1 className="name-label">Amanda Woldseth Markovic</h1>
+
+          <span>17 desember 1996 | Mylskerudveien 37, 1152 Oslo</span>
+          <br />
+          <span>
+            <a href="mailto:awmarkovic@gmail.com">awmarkovic@gmail.com</a>
+            <span> | </span>
+            <a href="tel:+4747668445">+47 47668445</a>
+          </span>
+        </div>
+      </header>
+      <div className="divider" />
+      <div className="left"></div>
+      <div className="right"></div>
+      <footer className="footer"></footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
